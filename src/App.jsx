18d934f274cac89798e18925f97e254858b7ba3c
koobawuah <me@bawuahboakye.com>
2022-05-07 11:24:02 +0000

    Initial tailwindcss setup and boilerplate code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p className="text-7xl">Hello Vite + React!</p>
       </header>
     </div>
   )
