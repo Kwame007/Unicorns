e6afcafabfc73025b1e91a19d40dacfe7fdb5645
koobawuah <me@bawuahboakye.com>
2022-05-10 17:13:29 +0000

    Added Contact.jsx page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Layout } from '../components'
+
+
+const Contact = () => {
+
+    return (
+        <>
+            <Layout>
+                <h1> You can contact us on</h1> 
+            </Layout>
+        </>
+        )
+}
+
+export default Contact
