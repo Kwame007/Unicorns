951f53f217a0f3d39ed157c7629b0de5c45daea7
koobawuah <me@bawuahboakye.com>
2022-05-10 17:24:44 +0000

    Imported and exported reviews pages to the entire application

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Home from './Home'
+import Contact from './Contact'
+
+
+import WriteReview from './reviews/WriteReview'
+import WriteUniReview from './reviews/WriteUniReview'
+import WriteCourseReview from './reviews/WriteCourseReview'
+
+
+
+export { Home, Contact, WriteReview, WriteUniReview, WriteCourseReview } 
