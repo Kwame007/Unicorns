1ba286b51a294f78bc48c6b81130a30373b6275d
koobawuah <me@bawuahboakye.com>
2022-05-10 17:25:46 +0000

    Updating App.jsx with Routing component, updating main.jsx to instantiate BrowserRouter from react-router-dom@6

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Routing from './routing'
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="font-semibold text-3xl">Unicorns</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-7xl">Hello Vite + React!</p>
-      </header>
+      <Routing />
     </div>
   )
 }
