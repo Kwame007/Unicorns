4889dbab77c9af92ec01fb7461200eb64f20a687
koobawuah <me@bawuahboakye.com>
2022-05-10 17:15:37 +0000

    Added Reviews pages

---
+++ src/pages/reviews/WriteCourseReview.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const WriteCourseReview = () => {
+
+    return (
+        <div> 
+            <h1>Reviewing [ course name ] </h1>
+        </div>
+
+    )
+
+}
+
+export default WriteCourseReview
---
+++ src/pages/reviews/WriteUniReview.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const WriteUniReview = () => {
+
+    return (
+        <div>
+            <h1> Reviewing  [ university name ] </h1>
+            </div>
+    )
+}
+
+export default WriteUniReview
