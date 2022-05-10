a2959507c91386c6dd4562743863a21e8f6d7708
koobawuah <me@bawuahboakye.com>
2022-05-10 17:10:26 +0000

    Initialising Routing

---
+++ src/routing/Routing.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { Contact, Home, WriteCourseReview, WriteUniReview, } from '../pages';
+
+export default function Routing() {
+
+    return (
+        <>
+            <Routes path='/'>
+                <Route index element={<Home />} />
+
+                <Route path='/reviews'>
+                    //Put reviews route here: To display list of reviews
+        
+
+                    <Route path='write-uni-review' element={<WriteUniReview /> } />
+                    <Route path='write-course-review' element={<WriteCourseReview /> } />
+                    
+                </Route>
+                <Route path='/contact' element={<Contact />}/>
+            </Routes>
+        </>
+    )
+}
+
