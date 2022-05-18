e88ec850e7c620f8f6aae3765bbf9d72cd60a27d
koobawuah <me@bawuahboakye.com>
2022-05-18 19:49:14 +0000

    Structure for WriteReview page accomplished

--- src/pages/reviews/WriteReview.jsx
+++ src/pages/reviews/WriteReview.jsx
@@ -6,6 +6,16 @@ const WriteReview = () => {
     <>
       <Layout>
         <h1> Write your reviews here </h1>
+        <div className="flex flex-row justify-around">
+            <select onSelect="">
+                <option value="">Default University</option>
+                <option value="UNI">University</option>
+            </select>
+            <select>
+                <option>Default Course</option>
+                <option value="">Course 101</option>
+            </select>
+        </div>
       </Layout>
     </>
   );
