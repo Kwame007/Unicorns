1fc7c47cd35dc8f14eb54b421ec11dbe1ead0687
koobawuah <me@bawuahboakye.com>
2022-05-19 21:41:28 +0000

    Completed top section of review

--- src/pages/reviews/WriteReview.jsx
+++ src/pages/reviews/WriteReview.jsx
@@ -1,21 +1,56 @@
 import React from "react";
 import { Layout } from "../../components";
+import { useQuery } from "@apollo/client";
+import { GET_UNICORNS } from "../../graphql/queries";
 
 const WriteReview = () => {
+
+    const [uni, setUni] = React.useState()
+    const [course, setCourse] = React.useState()
+    const [uniData, setUniData] = React.useState([])
+    const [courseData, setCourseData] = React.useState([])
+    const { loading, data, error } = useQuery(GET_UNICORNS, 
+    {
+        onCompleted: (data) => {
+            data && (
+                setUniData(data?.universities)
+            ),
+            (
+                setCourseData(data?.courses)
+            )
+        },
+    })
+
+
   return (
     <>
       <Layout>
         <h1> Write your reviews here </h1>
         <div className="flex flex-row justify-around">
-            <select onSelect="">
-                <option value="">Default University</option>
-                <option value="UNI">University</option>
+            <select value={uni} onChange={ (e) => (setUni(e.target.value)) }>
+                <option value="">Select University</option>
+                { 
+                    uniData && ( uniData.map( ({name, slug}, index) => ( <option key={index} value={slug}>{name}</option>) ) )
+                }
             </select>
-            <select>
-                <option>Default Course</option>
-                <option value="">Course 101</option>
+            <select value={course} onChange={ (e) => (setCourse(e.target.value)) }>
+                <option>Select Course</option>
+                {
+                    uni
+                    ?(
+                        courseData.filter((item) => ( uni === item.courseAt.slug) ).map( ({courseTitle, courseCode}, index) => (<option key={index} value={courseCode}>{courseTitle}</option>) )
+                    )
+                    :(
+                        courseData.map( ({courseCode, courseTitle}, index) => (
+                            <option key={index} value={courseCode}>{courseTitle}</option>
+                        ))
+                    )
+                }
             </select>
         </div>
+
+
+            <p>You selected {uni}</p>
       </Layout>
     </>
   );
