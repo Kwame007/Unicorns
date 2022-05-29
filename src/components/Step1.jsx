51f89ef4e1e8f24afe662b20d499f9f598cce85a
koobawuah <me@bawuahboakye.com>
2022-05-29 00:02:54 +0000

    Refactoring

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useRef} from 'react'
-import { RatingButton } from './RatingButton'
+import React, {useState, useRef, useEffect} from 'react'
+import RatingButton from './RatingButton'
 
 function Step1() {
 
