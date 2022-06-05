fac12cf060abec2a211f86b0bd877cdc1404c8d3
koobawuah <me@bawuahboakye.com>
2022-06-05 14:50:55 +0000

    Imported and exported all new components to the entire application

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,14 +2,13 @@ import Layout from "./Layout";
 import RatingButton from "./RatingButton";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
+import Step3 from "./Step3";
+import Step4 from "./Step4";
 import StepIndicator from "./StepIndicator";
 
-export { Layout, RatingButton, Step1, Step2, StepIndicator  } 
+export { Layout, RatingButton, Step1, Step2, Step3, Step4, StepIndicator  } 
 
 
-//add new components
-//forms
-//step2 input field forms for Pros Cons Summary
 //step3 forms for Alumni, and user or anonymous 
 
 //add submit button to step indicator component
