b65916f3ee7fc55fb420cc1cceba325d4464f565
Bawuah Boakye <me@bawuahboakye.com>
2022-06-13 23:33:24 +0000

    More Tasks To Do

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,9 +7,9 @@ import StepIndicator from "./StepIndicator";
 export { Layout, RatingButton, Step1, Step2, StepIndicator  } 
 
 
-//add new components
-//forms
-//step2 input field forms for Pros Cons Summary
+
 //step3 forms for Alumni, and user or anonymous 
 
 //add submit button to step indicator component
+ 
+//redux store config  
