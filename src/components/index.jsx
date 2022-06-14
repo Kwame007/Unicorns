32eb3f7be6a89df2fa13c4d08729170941bc32aa
Bawuah Boakye <me@bawuahboakye.com>
2022-06-14 23:03:26 +0000

    Removed completed task 3

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,9 +6,7 @@ import StepIndicator from "./StepIndicator";
 
 export { Layout, RatingButton, Step1, Step2, StepIndicator  } 
 
-
-
-//step3 forms for Alumni, and user or anonymous 
+ 
 
 //add submit button to step indicator component
  
