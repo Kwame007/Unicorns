567f3521fdf8679483036314e5689f2192276606
koobawuah <me@bawuahboakye.com>
2022-06-17 17:03:02 +0000

    feat: Added reducers functions to mutate global state in the reviewSlice

--- src/features/review/reviewSlice.jsx
+++ src/features/review/reviewSlice.jsx
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    univeristy: 'university',
+    university: 'university',
     course: 'course',
+    overallRating: 0,
     courseRating: 0,
     facultyRating: 0,
     facilitiesRating: 0,
@@ -18,8 +19,45 @@ const initialState = {
 export const reviewSlice = createSlice({
     name: 'review',
     initialState,
-    reducers: {},
+    reducers: {
+        setUniversity: (state, action) => {
+            state.university = action.payload
+        },
+        course: (state, action) => {
+            state.course = action.payload
+        },
+        overallRating: (state, action) => {
+            state.overallRating = action.payload
+        },
+        ratingCourse: (state, action) => {
+            state.courseRating = action.payload
+        },
+        ratingFaculty: (state, action) => {
+            state.facultyRating = action.payload
+        },
+        ratingFactilities: (state, action) => {
+            state.facilitiesRating = action.payload
+        },
+        ratingRecommendation: (state, action) => {
+            state.recommendationRating = action.payload
+        },
+        setPros: (state, action) => {
+            state.pros = action.payload
+        },
+        setCons: (state, action) => {
+            state.cons = action.payload
+        }, 
+        setGradYear: (state, action) => {
+            state.gradYear = action.payload
+        },
+        isAnonTrue: (state, action) => {
+            state.isAnon = !state.isAnon
+        },
+        setAlias: (state, action) => {
+            state.alias = action.payload
+        }
+    },
 })
 
-
+export const { setUniversity, course, ratingCourse, ratingFaculty, ratingFactilities, ratingRecommendation, setPros, setCons, setGradYear, isAnonTrue, setAlias } = reviewSlice.actions
 export default reviewSlice.reducer
