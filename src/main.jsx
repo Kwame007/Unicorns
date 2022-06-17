42decd39894c111f72996a75a05581b736882b8b
koobawuah <me@bawuahboakye.com>
2022-06-17 15:55:43 +0000

    feat: Added Provider component from react-redux library to expose redux global state(the configured store) to the entire application

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 // apollo client
 const client = new ApolloClient({
@@ -13,8 +15,10 @@ const client = new ApolloClient({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </ApolloProvider>
 );
