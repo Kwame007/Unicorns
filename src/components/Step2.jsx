44c726dd733e55dcd04f15cd3061c851513ba4c6
koobawuah <me@bawuahboakye.com>
2022-07-01 13:53:51 +0000

    (feat): Used global reducer actions/functions to save the input text of pros, cons and reviewSummary into the global state

--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import {setPros} from '../features/review/reviewSlice'
+import {setPros, setCons, setReviewSummary, } from '../features/review/reviewSlice'
 
 const Step2 = () => {
 
+    const prosRef = React.useRef(null)
+    const consRef= React.useRef(null)
+    const summaryRef = React.useRef(null)
     const dispatch = useDispatch()
 
     return (
@@ -14,16 +17,16 @@ const Step2 = () => {
                 <div className="w-full flex flex-col space-y-6 md:space-y-0 md:flex-row md:gap-x-4"> 
                     <div className="flex-1">
                         <p className="max-w-md text-left pt-2 text-lg text-slate-500 font-normal leading-5">Write about positive experiences you had while in this university. Short, concise and helpful information.</p>
-                        <input className="w-full mt-4 px-5 py-2 border-2 border-black focus:outline-indigo-500 focus:ring-indigo-200 focus:ring-2 text-xl  placeholder:font-semibold placeholder:text-slate-500 placeholder:text-xl" type="text" placeholder="Pros" onSubmit={ (e) => {  console.log(e.target.value) }}/>
+                        <input className="w-full mt-4 px-5 py-2 border-2 border-black focus:outline-indigo-500 focus:ring-indigo-200 focus:ring-2 text-xl  placeholder:font-semibold placeholder:text-slate-500 placeholder:text-xl" type="text" placeholder="Pros" onChange={ () => dispatch(setPros(prosRef.current.value))} ref={prosRef}/>
                     </div>
                     <div className="flex-1"> 
                         <p className="max-w-md text-left pt-0  md:pt-2 text-lg text-slate-500 font-normal leading-5">Write your negative experiences you had while in this university. Short, concise and helpful information.</p>
-                        <input className="w-full mt-4 px-5 py-2 border-2 border-black focus-outline-indigo-500 focus:ring-indigo-200 focus:ring-2 text-xl placeholder:font-semibold placeholder:text-slate-500 placeholder:text-xl" type="text" placeholder="Cons"/>
+                        <input className="w-full mt-4 px-5 py-2 border-2 border-black focus:outline-indigo-500 focus:ring-indigo-200 focus:ring-2 text-xl placeholder:font-semibold placeholder:text-slate-500 placeholder:text-xl" type="text" placeholder="Cons" onChange={ () => dispatch(setCons(consRef.current.value))} ref={consRef}/>
                     </div>
                 </div>
                 <div className="w-full my-6"> 
                     <p className="max-w-lg text-left pt-2 text-lg text-slate-500 font-normal leading-5">Write your total university experience while you were enrolled in this university. </p>
-                    <textarea className="w-full h-40 mt-4 p-5 border-2 border-black text-xl  focus:outline-indigo-500 focus:ring-indigo-200 focus:ring-2 placeholder:font-semibold placeholder:text-slate-500 placeholder:text-xl" type="text" placeholder="Please provide your review summary here..."/>
+                    <textarea className="w-full h-40 mt-4 p-5 border-2 border-black text-xl  focus:outline-indigo-500 focus:ring-indigo-200 focus:ring-2 placeholder:font-semibold placeholder:text-slate-500 placeholder:text-xl" type="text" placeholder="Please provide your review summary here..." onChange={ () => dispatch(setReviewSummary(summaryRef.current.value))} ref={summaryRef}/>
                 </div>
             </form>
             </div>
