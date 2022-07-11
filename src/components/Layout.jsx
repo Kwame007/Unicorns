2a3b5d3192aa7503d413a8ae0f412af092b899e0
koobawuah <me@bawuahboakye.com>
2022-07-11 13:40:06 +0000

    feat: Added footer to layout component to apply to all pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -58,7 +58,12 @@ function Header() {
 
 //footer
 function Footer() {
-  return <></>;
+  const d = new Date()
+  return (
+        <div className="w-full h-28 py-20 bg-red-50 flex flex-col justify-center">
+            <p className="font-black">Unicorns</p>  <span>All Rights Reserved &copy; {d.getFullYear()}</span> <p>&lt;Development lead=&quot;Clement&quot; junior=&quot;<a href="https://bawuahboakye.com">Bawuahboakye</a>&quot; /&gt;</p>
+        </div>
+  )
 }
 
 //page layout order
