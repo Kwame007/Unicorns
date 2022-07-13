ea7e51ce5b3f195d0bfc7c0457f10330dca8be27
Kwame007 <clementnyankuadjei@gmail.com>
2022-07-13 17:01:19 +0000

    add paragraph clamp feature

--- src/pages/reviews/ReviewsList.jsx
+++ src/pages/reviews/ReviewsList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { motion } from "framer-motion";
 
@@ -37,9 +37,18 @@ const ReviewsList = () => {
 
   console.log(data);
 
+  const [fullText, setFullText] = useState(false);
+
+  // show full text
+  const showFullText = () => {
+    setFullText(true);
+  };
+
   return (
-    <div className="container mx-auto mt-6 p-5 bg-gray-100">
-      <h2 className="text-3xl font-bold text-center my-12">Recent Reviews</h2>
+    <div className="container mx-auto mt-6 mb-6 p-5 ">
+      <h2 className="text-3xl font-bold text-center my-12  mb-10  md:mb-20 md:text-4xl">
+        Recent Reviews
+      </h2>
       <div className="grid grid-cols-1 gap-8 justify-center items-center w-full hover:cursor-pointer md:grid-cols-3 ">
         {/* {!error && <h4>error occured 👺</h4>} */}
         {loading && <h3>Loading</h3>}
@@ -56,12 +65,25 @@ const ReviewsList = () => {
                 <div className="flex">
                   <h3 className="text-lg font-bold">{review.reviewSummary}</h3>
                 </div>
-                <p className="text-left overflow-auto leading-6 text-gray-400 text-lg py-5">
+                <p
+                  className={`text-left text-gray-500 text-base ${
+                    !fullText ? "line-clamp-4" : "line-clamp-none"
+                  }`}
+                >
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
-                  vitae eum. Error placeat vel qui magni quia eum odit
-                  excepturi! Autem tenetur architecto, ratione quod nostrum
-                  explicabo sapiente facere perspiciatis?
+                  excepturi quidem. Error reprehenderit tempora culpa dicta
+                  voluptatibus provident consequuntur est a architecto iusto
+                  illum magni sequi laborum nam ad vitae ea eos earum accusamus,
+                  quaerat assumenda commodi saepe? Fugiat quasi beatae quia ut
+                  perspiciatis sunt ducimus cupiditate quas voluptatem possimus.
                 </p>
+                <button
+                  type="button"
+                  className="text-left text-sm w-28 text-blue-500 inline py-5 font-bold hover:underline"
+                  onClick={showFullText}
+                >
+                  See More
+                </button>
                 <aside className="flex flex-row justify-start items-center">
                   <span className="bg-gray-100 p-4 rounded-full mr-5"></span>
                   <small>
