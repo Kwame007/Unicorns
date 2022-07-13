cf51464bc7b6f37b5a93e91d9b181c3660293b02
Kwame007 <clementnyankuadjei@gmail.com>
2022-07-13 17:03:08 +0000

    remove components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { Layout } from "../components";
-import ReviewsList from "./reviews/ReviewsList";
 
 const Home = () => {
   return (
     <>
-      <Layout>
-        <header className="App-header">
-          {/* <p className="text-7xl">testing</p> */}
-        </header>
-        <ReviewsList />
-      </Layout>
+      <Layout></Layout>
     </>
   );
 };
