934047499367eaff6d322d9ebc687f8e363a6bb3
Kwame007 <clementnyankuadjei@gmail.com>
2022-07-13 16:53:06 +0000

    import and export added components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,9 +5,30 @@ import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
 import StepIndicator from "./StepIndicator";
+import Input from "./Input";
+import Card from "./Card";
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import TrendingUni from "./TrendingUni";
+import ReviewGuide from "./ReviewGuide";
 
-export { Layout, RatingButton, Step1, Step2, Step3, Step4, StepIndicator };
+export {
+  Layout,
+  RatingButton,
+  Step1,
+  Step2,
+  Step3,
+  Step4,
+  StepIndicator,
+  Input,
+  Card,
+  Footer,
+  Header,
+  Hero,
+  TrendingUni,
+  ReviewGuide,
+};
 
-
-// Work on step indicator component  
-// Post review to backend 
+// Work on step indicator component
+// Post review to backend
