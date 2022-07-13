cafdde8f872d54ba7d9a75af3ff890a4ee19df12
Kwame007 <clementnyankuadjei@gmail.com>
2022-07-13 17:01:59 +0000

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-  content: [
-      './index.html',
-      './src/**/*.{vue,js,jsx,ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern":
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('./src/assets/images/uni-bg.jpg')",
+        gradient:
+          " linear-gradient(to bottom, #6366f1, #928af6, #b9b0fa, #ddd7fd, #ffffff);",
+      },
+      backgroundColor: {
+        light: "#f5f6f",
+      },
+      boxShadow: {
+        light: "0px 1px 6px rgba(0, 0, 0, 0.1)",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
