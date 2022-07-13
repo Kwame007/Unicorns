c0ae93195ee9a8f6612ee0318f033d68a7e65c4e
Kwame007 <clementnyankuadjei@gmail.com>
2022-07-13 23:22:17 +0000

    import and export landing page component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,8 @@ import Contact from './Contact'
 import WriteReview from './reviews/WriteReview'
 import WriteUniReview from './reviews/WriteUniReview'
 import WriteCourseReview from './reviews/WriteCourseReview'
+import LandingPage from '../pages/LandingPage'
 
 
 
-export { Home, Contact, WriteReview, WriteUniReview, WriteCourseReview } 
+export { Home, Contact, WriteReview, WriteUniReview, WriteCourseReview, LandingPage  } 
