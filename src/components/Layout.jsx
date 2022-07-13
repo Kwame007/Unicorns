bfc567efa79781876404cccd6117313c1d62ce7e
Kwame007 <clementnyankuadjei@gmail.com>
2022-07-13 23:20:07 +0000

    remove components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 
 import Footer from "./Footer";
 import Header from "./Header";
-import Hero from "./Hero";
-import TrendingUni from "./TrendingUni";
-import ReviewGuide from "./ReviewGuide";
-import ReviewsList from "../pages/reviews/ReviewsList";
 
 //page layout order
 const Layout = ({ children }) => {
@@ -15,10 +11,6 @@ const Layout = ({ children }) => {
       <Header />
       <Outlet />
       {children}
-      <Hero />
-      <TrendingUni />
-      <ReviewGuide />
-      <ReviewsList />
       <Footer />
     </div>
   );
